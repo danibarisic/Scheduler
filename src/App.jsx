412f52cd3ad49ef4e8d61cdade616df5f98bed21
Dani Barisic <barisicdani@gmail.com>
2025-03-27 16:39:59 +0100

    Debugging.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 import './App.css';
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+
+// Fetching data from API.
+const fetchSchedule = async () => {
+  const url = "https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php";
+  const response = await fetch(url);
+  if (!response.ok) throw response;
+  return await response.json();
+};
 
 // Create an object with title and course inforation.
 const schedule = {
@@ -35,7 +44,7 @@ const Banner = ({ title }) => (
 
 const CourseList = ({ courses }) => (
   <div className="course-list">
-    {Object.values(courses).map(course => <Course course={course} />)}
+    {Object.values(courses).map(course => <Course course={course} key={course.id} />)}
   </div>
 );
 
@@ -62,12 +71,30 @@ const Course = ({ course }) => (
   </div>
 );
 
-// Constructs the app.
+const Main = () => {
+  const { schedule: data, isLoading, error } = useQuery({
+    queryKey: ['schedule'],
+    queryFn: fetchSchedule
+  });
+
+  if (error) return <h1>{error}</h1>;
+  if (isLoading) return <h1>Loading the schedule...</h1>
+
+  console.log(data);
+  return (
+    <div className="container">
+      <Banner title={schedule.title} />
+      <CourseList courses={schedule.courses} />
+    </div>
+  );
+};
+
+const queryClient = new QueryClient();
+
 const App = () => (
-  <div className="container">
-    <Banner title={schedule.title} />
-    <CourseList courses={schedule.courses} />
-  </div>
+  <QueryClientProvider client={queryClient}>
+    <Main />
+  </QueryClientProvider>
 );
 
 export default App;
