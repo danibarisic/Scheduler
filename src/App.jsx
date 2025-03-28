e5adb85afc5bf7abb6907ed13cf7ed9334a53d40
Dani Barisic <barisicdani@gmail.com>
2025-03-28 17:08:44 +0100

    Added functions to prevent course time conficts

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const fetchSchedule = async () => {
   const url = "https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php";
   const response = await fetch(url);
   if (!response.ok) throw response;
-  return await response.json();
+  return addScheduleTimes(await response.json());
 };
 
 // Create an object with title and course inforation.
@@ -43,18 +43,23 @@ const Banner = ({ title }) => (
 );
 
 const CourseList = ({ courses }) => {
-  const [term, setTerm] = useState('Spring');
+  const [term, setTerm] = useState('Fall');
+  const [selected, setSelected] = useState([]);
   const termCourses = Object.values(courses).filter(course => term === getCourseTerm(course));
 
   return (
     <>
       <TermSelector term={term} setTerm={setTerm} />
       <div className="course-list">
-        {termCourses.map(course => <Course course={course} key={course.id} />)}
+        {termCourses.map(course => <Course course={course} key={course.id} selected={selected} setSelected={setSelected} />)}
       </div>
     </>
   )
 };
+
+const toggle = (x, lst) => (
+  lst.includes(x) ? lst.filter(y => y !== x) : [x, ...lst]
+);
 
 const TermButton = ({ term, setTerm, checked }) => (
   <>
@@ -87,16 +92,76 @@ const getCourseNumber = course => (
   course.id.slice(1, 4)
 );
 
-const Course = ({ course }) => (
-  <div className="card m-1 p-2">
-    <div className="card-body">
-      <div className="card-title fs-4">{getCourseTerm(course)} CS {getCourseNumber(course)}</div>
-      <div className="card-text">{course.title}</div>
-      <hr></hr>
-      <div className="card-text">{course.meets}</div>
-    </div>
-  </div>
+// Check if there is conflict between course and selected course.
+const hasConflict = (course, selected) => (
+  selected.some(selection => courseConflict(course, selection))
 );
+
+const days = ['M', 'Tu', 'W', 'Th', 'F'];
+
+const daysOverlap = (days1, days2) => {
+  if (!days1 || !days2) return false;
+  return days.some(day => days1.includes(day) && days2.includes(day))
+};
+
+const hoursOverlap = (hours1, hours2) => (
+  Math.max(hours1.start, hours2.start) < Math.min(hours1.end, hours2.end)
+);
+
+const timeConflict = (course1, course2) => (
+  daysOverlap(course1.days, course2.days) && hoursOverlap(course1.hours, course2.hours)
+);
+
+const courseConflict = (course1, course2) => (
+  getCourseTerm(course1) === getCourseTerm(course2) && timeConflict(course1, course2)
+);
+
+const meetsPat = /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
+
+const timeParts = meets => {
+  const [match, days, hh1, mm1, hh2, mm2] = meetsPat.exec(meets) || [];
+  return !match ? {} : {
+    days,
+    hours: {
+      start: hh1 * 60 + mm1 * 1,
+      end: hh2 * 60 + mm2 * 1
+    }
+  };
+};
+
+const addCourseTimes = course => ({
+  ...course,
+  ...timeParts(course.meets)
+});
+
+const addScheduleTimes = schedule => ({
+  title: schedule.title,
+  courses: mapValues(addCourseTimes, schedule.courses)
+});
+
+const mapValues = (fn, obj) => (
+  Object.fromEntries(Object.entries(obj).map(([key, value]) => [key, fn(value)]))
+);
+
+const Course = ({ course, selected, setSelected }) => {
+  const isSelected = selected.includes(course);
+  const isDisabled = !isSelected && hasConflict(course, selected);
+  const style = { backgroundColor: isDisabled ? 'lightgrey' : isSelected ? 'lightgreen' : 'white' };
+
+  return (
+    <div className="card m-1 p-2"
+      style={style}
+      onClick={isDisabled ? null : () => setSelected(toggle(course, selected))}>
+      <div className="card-body">
+        <div className="card-title fs-4">{getCourseTerm(course)} CS {getCourseNumber(course)}</div>
+        <div className="card-text">{course.title}</div>
+        <hr></hr>
+        <div className="card-text">{course.meets}</div>
+      </div>
+    </div>
+  );
+}
+
 
 const Main = () => {
   const { schedule: data, isLoading, error } = useQuery({
