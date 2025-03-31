9dbc36234b415ab57d2ac6b59cf8d2d133a686f1
Dani Barisic <barisicdani@gmail.com>
2025-03-31 10:02:24 +0200

    Modularized the code into two files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+import { hasConflict, days, daysOverlap, hoursOverlap, timeConflict, courseConflict, meetsPat, timeParts, addCourseTimes, mapValues, addScheduleTimes } from './utilities/times.js';
 
 // Fetching data from API.
 const fetchSchedule = async () => {
@@ -92,57 +93,6 @@ const getCourseNumber = course => (
   course.id.slice(1, 4)
 );
 
-// Check if there is conflict between course and selected course.
-const hasConflict = (course, selected) => (
-  selected.some(selection => courseConflict(course, selection))
-);
-
-const days = ['M', 'Tu', 'W', 'Th', 'F'];
-
-const daysOverlap = (days1, days2) => {
-  if (!days1 || !days2) return false;
-  return days.some(day => days1.includes(day) && days2.includes(day))
-};
-
-const hoursOverlap = (hours1, hours2) => (
-  Math.max(hours1.start, hours2.start) < Math.min(hours1.end, hours2.end)
-);
-
-const timeConflict = (course1, course2) => (
-  daysOverlap(course1.days, course2.days) && hoursOverlap(course1.hours, course2.hours)
-);
-
-const courseConflict = (course1, course2) => (
-  getCourseTerm(course1) === getCourseTerm(course2) && timeConflict(course1, course2)
-);
-
-const meetsPat = /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
-
-const timeParts = meets => {
-  const [match, days, hh1, mm1, hh2, mm2] = meetsPat.exec(meets) || [];
-  return !match ? {} : {
-    days,
-    hours: {
-      start: hh1 * 60 + mm1 * 1,
-      end: hh2 * 60 + mm2 * 1
-    }
-  };
-};
-
-const addCourseTimes = course => ({
-  ...course,
-  ...timeParts(course.meets)
-});
-
-const addScheduleTimes = schedule => ({
-  title: schedule.title,
-  courses: mapValues(addCourseTimes, schedule.courses)
-});
-
-const mapValues = (fn, obj) => (
-  Object.fromEntries(Object.entries(obj).map(([key, value]) => [key, fn(value)]))
-);
-
 const Course = ({ course, selected, setSelected }) => {
   const isSelected = selected.includes(course);
   const isDisabled = !isSelected && hasConflict(course, selected);
@@ -161,7 +111,6 @@ const Course = ({ course, selected, setSelected }) => {
     </div>
   );
 }
-
 
 const Main = () => {
   const { schedule: data, isLoading, error } = useQuery({
